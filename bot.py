f149e422a1999ed21bd6ac9ba35a718cf3a1e526
OMINDA DILSHAN RESPACT <85678266+omindadelshan@users.noreply.github.com>
2021-10-07 20:55:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,10 @@
+import json
 import os
 import requests as re
+from gtts import gTTS
+from telegram import ChatAction
+from telegram.ext import run_async
+
 from bs4 import BeautifulSoup
 from pyrogram import Client, filters
 from pyrogram.types import (
@@ -31,12 +36,67 @@ def start(client, message):
 
 @app.on_message(filters.command(['help']))
 def start(client, message):
-            message.reply_text(text =f"Hello **what bn** \n\n **Iam Simple webper** ðŸ•¸ \n __SEND ME WEBSITE LINK AND GET THAT WEB SOURCE__",reply_to_message_id = message.message_id , parse_mode="markdown", reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("Support" ,url="https://t.me/lntechnical") ],
-                 [InlineKeyboardButton("Subscribe", url="https://youtube.com/c/LNtechnical") ]          ]        ) )
-                 
+@run_async
+@send_action(ChatAction.RECORD_AUDIO)
+def gtts(update, context):
+    msg = update.effective_message
+    reply = " ".join(context.args)
+    if not reply:
+        if msg.reply_to_message:
+            reply = msg.reply_to_message.text
+        else:
+            return msg.reply_text(
+                "Reply to some message or enter some text to convert it into audio format!"
+            )
+        for x in "\n":
+            reply = reply.replace(x, "")
+    try:
+        tts = gTTS(reply)
+        tts.save("k.mp3")
+        with open("k.mp3", "rb") as speech:
+            msg.reply_audio(speech)
+    finally:
+        if os.path.isfile("k.mp3"):
+            os.remove("k.mp3")
+
+
+# Open API key
+API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
+URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
+
+
+@run_async
+@typing_action
+def spellcheck(update, context):
+    if update.effective_message.reply_to_message:
+        msg = update.effective_message.reply_to_message
+
+        params = dict(lang="US", clientVersion="2.0", apiKey=API_KEY, text=msg.text)
+
+        res = requests.get(URL, params=params)
+        changes = json.loads(res.text).get("LightGingerTheTextResult")
+        curr_string = ""
+        prev_end = 0
+
+        for change in changes:
+            start = change.get("From")
+            end = change.get("To") + 1
+            suggestions = change.get("Suggestions")
+            if suggestions:
+                sugg_str = suggestions[0].get("Text")  # should look at this list more
+                curr_string += msg.text[prev_end:start] + sugg_str
+                prev_end = end
+
+        curr_string += msg.text[prev_end:]
+        update.effective_message.reply_text(curr_string)
+    else:
+        update.effective_message.reply_text(
+            "Reply to some message to get grammar corrected text!"
+        )
+
+
+dispatcher.add_handler(DisableAbleCommandHandler("tts", gtts, pass_args=True))
+dispatcher.add_handler(DisableAbleCommandHandler("splcheck", spellcheck))
 @app.on_message(filters.regex("^(http|https|www\.)"))
 def start(client, message):
     ms = message.reply_text("```Trying to web scrap .........```", reply_to_message_id = message.message_id)
